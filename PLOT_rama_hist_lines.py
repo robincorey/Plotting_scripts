ff28f90c3ed125730735494bc3dbcb782ad8b224
Robin Corey <robin.corey@gmail.com>
2017-11-10 11:27:13 +0000

    some more ramach stuff

--- PLOT_rama_hist_lines.py
+++ PLOT_rama_hist_lines.py
@@ -38,11 +38,15 @@ plt.rcParams['xtick.major.width'] = 2
 plt.rcParams['ytick.major.size'] = 5
 plt.rcParams['ytick.major.width'] = 2
 plt.rcParams['font.sans-serif'] = "cmss10"
-plt.rcParams['axes.unicode_minus']=False
 plt.rcParams['axes.facecolor'] = plt.cm.jet(0)
+plt.rcParams['axes.unicode_minus']=False
 plt.xticks(np.arange(-180, 181, 90))
 plt.yticks(np.arange(-180, 181, 90))
 plt.hist2d(xdata, ydata, (360, 360), cmap=plt.cm.jet)
+plt.plot([59, 59], [99, 179], 'w--', lw=2)
+plt.plot([109, 109], [99, 179], 'w--', lw=2)
+plt.plot([59, 109], [99, 99], 'w--', lw=2)
+plt.plot([59, 109], [179, 179], 'w--', lw=2)
 plt.xlim([-180,180])
 plt.ylim([-180,180])
 strip1 = filename.rstrip('.xvg')
@@ -50,4 +54,5 @@ sysname = strip1.replace('_',' ')
 plt.text(60, -160, '%s' % sysname, color='white', fontsize=15)
 plt.xlabel('%s' % xlabel, fontname="cmss10", fontsize=25 )
 plt.ylabel('%s' % ylabel, fontname="cmss10", fontsize=25 )
-plt.savefig('%s.png' % strip1, bbox_inches='tight')
+plt.savefig('%s_lines.png' % strip1, bbox_inches='tight')
+print datetime.now() - startTime
