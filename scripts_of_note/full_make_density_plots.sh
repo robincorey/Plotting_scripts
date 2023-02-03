85fff19dbeb06dbc66090f19d402fb9d7048c57d
robincorey <robin.corey@gmail.com>
2023-02-03 14:34:49 +0000

    script for running density analyses

--- scripts_of_note/full_make_density_plots.sh
+++ scripts_of_note/full_make_density_plots.sh
@@ -1,36 +1,38 @@
+#!/bin/bash
+
 ## run all of the scripts needed to make lipid density plots 
 #takes input $1 number of repeats
+## if not working try source /.bashrc before running
 
-for i in $(seq 1 $1)
+GIT=/sansom/s137/bioc1535/Desktop/git-repos/Plotting_scripts/scripts_of_note/
+
+module load ubuntu-18/plumed/2.5.0
+
+mkdir -p DENS
+
+echo -e aBB '\n' q | gmx make_ndx -f md_1.tpr -o BB.ndx
+
+for lipid in POPE BB CARD POPG
+do 
+	echo $lipid | gmx editconf -f md_1.gro -o md-centre-$lipid.pre.pdb -n  BB.ndx
+	echo $lipid | gmx editconf -f md-centre-$lipid.pre.pdb -o md-centre-$lipid.pdb
+	for i in $(seq 1 $1)
+	do 
+		echo "Processing md $i"
+		echo -e BB '\n' System | gmx trjconv -f md_$i.xtc -s md_$i.tpr -center -o DENS/centre.$lipid.$i.xtc -n BB.ndx -skip 10 -pbc mol
+		echo -e BB '\n' $lipid | gmx trjconv -f DENS/centre.$lipid.$i.xtc -s md_$i.tpr -fit rotxy+transxy -n BB.ndx -o DENS/$lipid.$i.xtc
+	done
+	gmx trjcat -f DENS/$lipid.*.xtc -o DENS/all.$lipid.xtc -cat
+	$GIT/make_plumed_script2.sh $lipid > DENS/${lipid}_plumed_coordinates.sh
+	sed -i "s/PRINT ,pos/PRINT ARG=pos/g" DENS/${lipid}_plumed_coordinates.sh
+	plumed driver --mf_xtc DENS/all.$lipid.xtc --plumed DENS/${lipid}_plumed_coordinates.sh
+done
+
+for lipid in POPE CARD POPG
 do
-echo "Processing MD$i"
-cd MD$i
-
-#first, fit the protein and output files for each lipid/the backbone
-
-#next make plumed script
-bash ~/Documents/Analysis-scripts/density_plots/make_plumed_script2.sh POPE > POPE_plumed_coordinates.sh
-bash ~/Documents/Analysis-scripts/density_plots/make_plumed_script2.sh POPG > POPG_plumed_coordinates.sh
-bash ~/Documents/Analysis-scripts/density_plots/make_plumed_script2.sh CARD > CARD_plumed_coordinates.sh
-bash ~/Documents/Analysis-scripts/density_plots/make_plumed_script2.sh BB > BB_plumed_coordinates.sh
-
-#edit last lines of lipid plumed scripts
-sed -i "s/PRINT ,pos/PRINT ARG=pos/g" POPE_plumed_coordinates.sh
-sed -i "s/PRINT ,pos/PRINT ARG=pos/g" POPG_plumed_coordinates.sh
-sed -i "s/PRINT ,pos/PRINT ARG=pos/g" CARD_plumed_coordinates.sh
-
-#run plumed driver to get positions
-plumed driver --mf_xtc md-centre-skip-POPE.xtc --plumed POPE_plumed_coordinates.sh
-plumed driver --mf_xtc md-centre-skip-POPG.xtc --plumed POPG_plumed_coordinates.sh
-plumed driver --mf_xtc md-centre-skip-CARD.xtc --plumed CARD_plumed_coordinates.sh
-plumed driver --mf_pdb md-centre-BB.pdb --plumed BB_plumed_coordinates.sh
-
-#make density plots
-#module purge
-python ~/Documents/Analysis-scripts/density_plots/lipid-density-xy-new.py
+	python $GIT/lipid-density-xy-new.py $lipid
+done
 
 rm ./bck*
-rm ./#*
-
-cd ../
-done
+rm ./*#*
+rm -f DENS/*#*
